2d98cebe0369ad98cfc59ebe2ef99d635145478e
Milena Rickli Silvério <141282223+MilenaRickliS@users.noreply.github.com>
2024-04-09 16:21:35 -0300

    c

---
+++ Jogo1/Assets/Scripts/AudioController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioSource fundoMusical;
+
+
+    public void VolumeMusical(float value){
+        fundoMusical.volume = value;
+    }
+}
